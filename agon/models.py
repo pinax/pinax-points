7b7a767cd64cd787266d7a060d7c7915d3ac9e64
Brian Rosner <brosner@gmail.com>
2010-04-15 08:21:30 -0600

    added positions calculation

--- agon/models.py
+++ agon/models.py
@@ -1,4 +1,5 @@
 import datetime
+import itertools
 
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
@@ -51,6 +52,32 @@ class TargetStat(models.Model):
         return cls._default_manager.filter(**lookup_params).update(
             points = models.F("points") + given,
         )
+    
+    @classmethod
+    def update_positions(cls, point_range=None):
+        queryset = cls._default_manager.order_by("-points")
+        
+        if point_range is not None:
+            # ensure point_range is always [0] < [1]
+            if point_range[0] > point_range[1]:
+                point_range = (point_range[1], point_range[0])
+            all_target_stats = queryset.filter(points__range=point_range)
+            position = queryset.filter(points__gt=point_range[1]).count()
+        else:
+            all_target_stats = queryset
+            position = 0
+        
+        grouped_target_stats = itertools.groupby(all_target_stats, lambda x: x.points)
+        prev_group_len = 0
+        
+        for points, target_stats in grouped_target_stats:
+            position += prev_group_len + 1
+            target_stats = list(target_stats)
+            prev_group_len = len(target_stats) - 1
+            pks = []
+            for target_stat in target_stats:
+                pks.append(target_stat.pk)
+            cls._default_manager.filter(pk__in=pks).update(position=position)
 
 
 def award_points(target, key):
@@ -85,6 +112,11 @@ def award_points(target, key):
             TargetStat.update_points(points_given, lookup_params)
     
     signals.points_awarded.send(sender=target.__class__, target=target, key=key)
+    
+    new_points = points_awarded(target)
+    old_points = new_points - points_given
+    
+    TargetStat.update_positions((old_points, new_points))
 
 
 def points_awarded(target):
--- agon/tests.py
+++ agon/tests.py
@@ -6,6 +6,7 @@ from django.test import TestCase, TransactionTestCase
 
 from django.contrib.auth.models import User, Group
 
+from agon.models import TargetStat
 from agon.models import award_points, points_awarded
 
 
@@ -18,16 +19,16 @@ def skipIf(cond):
 
 class BasePointsTestCase(object):
     
-    def setUp(self):
-        self.users = [
-            User.objects.create_user(
-                "user_%d" % i, "user_%d@example.com" % i, str(i)
-            ) for i in xrange(1)
-        ]
-    
     def tearDown(self):
         if hasattr(settings, "AGON_POINT_VALUES"):
             del settings.AGON_POINT_VALUES
+    
+    def setup_users(self, N):
+        self.users = [
+            User.objects.create_user(
+                "user_%d" % i, "user_%d@example.com" % i, str(i)
+            ) for i in xrange(N)
+        ]
     
     def setup_points(self, value):
         settings.AGON_POINT_VALUES = value
@@ -36,6 +37,7 @@ class BasePointsTestCase(object):
 class PointsTestCase(BasePointsTestCase, TestCase):
     
     def test_improperly_configured(self):
+        self.setup_users(1)
         user = self.users[0]
         try:
             award_points(user, "JOINED_SITE")
@@ -48,6 +50,7 @@ class PointsTestCase(BasePointsTestCase, TestCase):
             self.assertEqual(str(e), "You must define a point value for 'JOINED_SITE'")
     
     def test_simple_user_point_award(self):
+        self.setup_users(1)
         self.setup_points({
             "JOINED_SITE": 1,
         })
@@ -68,6 +71,7 @@ class PointsTransactionTestCase(BasePointsTestCase, TransactionTestCase):
     
     @skipIf(settings.DATABASE_ENGINE == "sqlite3")
     def test_concurrent_award(self):
+        self.setup_users(1)
         user = self.users[0]
         self.setup_points({
             "INVITED_USER": 10,
@@ -82,3 +86,68 @@ class PointsTransactionTestCase(BasePointsTestCase, TransactionTestCase):
         for t in threads:
             t.join()
         self.assertEqual(points_awarded(user), 50)
+
+
+class PositionsTestCase(BasePointsTestCase, TestCase):
+    
+    def test_no_range(self):
+        self.setup_users(9)
+        
+        TargetStat.objects.create(target_user=self.users[0], points=100)
+        TargetStat.objects.create(target_user=self.users[1], points=90)
+        TargetStat.objects.create(target_user=self.users[2], points=85)
+        TargetStat.objects.create(target_user=self.users[3], points=70)
+        TargetStat.objects.create(target_user=self.users[4], points=70)
+        TargetStat.objects.create(target_user=self.users[5], points=60)
+        TargetStat.objects.create(target_user=self.users[6], points=50)
+        TargetStat.objects.create(target_user=self.users[7], points=10)
+        TargetStat.objects.create(target_user=self.users[8], points=5)
+        
+        TargetStat.update_positions()
+        
+        self.assertEqual(
+            [(p.position, p.points) for p in TargetStat.objects.order_by("position")],
+            [(1, 100), (2, 90), (3, 85), (4, 70), (4, 70), (6, 60), (7, 50), (8, 10), (9, 5)]
+        )
+    
+    def test_up_range(self):
+        self.setup_users(9)
+        
+        TargetStat.objects.create(target_user=self.users[0], points=100, position=1)
+        TargetStat.objects.create(target_user=self.users[1], points=90, position=2)
+        TargetStat.objects.create(target_user=self.users[2], points=85, position=3)
+        TargetStat.objects.create(target_user=self.users[3], points=70, position=4)
+        TargetStat.objects.create(target_user=self.users[4], points=70, position=4)
+        TargetStat.objects.create(target_user=self.users[5], points=60, position=6)
+        TargetStat.objects.create(target_user=self.users[6], points=61, position=7)
+        TargetStat.objects.create(target_user=self.users[7], points=10, position=8)
+        TargetStat.objects.create(target_user=self.users[8], points=5, position=9)
+        
+        # user 6 scored 11 points
+        TargetStat.update_positions((50, 61))
+        
+        self.assertEqual(
+            [(p.position, p.points) for p in TargetStat.objects.order_by("position")],
+            [(1, 100), (2, 90), (3, 85), (4, 70), (4, 70), (6, 61), (7, 60), (8, 10), (9, 5)]
+        )
+    
+    def test_down_range(self):
+        self.setup_users(9)
+        
+        TargetStat.objects.create(target_user=self.users[0], points=100, position=1)
+        TargetStat.objects.create(target_user=self.users[1], points=90, position=2)
+        TargetStat.objects.create(target_user=self.users[2], points=69, position=3)
+        TargetStat.objects.create(target_user=self.users[3], points=70, position=4)
+        TargetStat.objects.create(target_user=self.users[4], points=70, position=4)
+        TargetStat.objects.create(target_user=self.users[5], points=60, position=6)
+        TargetStat.objects.create(target_user=self.users[6], points=50, position=7)
+        TargetStat.objects.create(target_user=self.users[7], points=10, position=8)
+        TargetStat.objects.create(target_user=self.users[8], points=5, position=9)
+        
+        # user 6 scored 11 points
+        TargetStat.update_positions((85, 69))
+        
+        self.assertEqual(
+            [(p.position, p.points) for p in TargetStat.objects.order_by("position")],
+            [(1, 100), (2, 90), (3, 70), (3, 70), (5, 69), (6, 60), (7, 50), (8, 10), (9, 5)]
+        )
