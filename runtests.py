c6b883f12e7b15c3d3a2c877a9176b747a49f29e
Brian Rosner <brosner@gmail.com>
2010-10-31 11:45:22 -0600

    corrected incorrect use of a tuple

--- runtests.py
+++ runtests.py
@@ -8,12 +8,12 @@ from django.conf import settings as django_settings
 if not django_settings.configured:
     django_settings.configure(
         DATABASE_ENGINE="sqlite3",
-        INSTALLED_APPS=(
+        INSTALLED_APPS=[
             "django.contrib.sessions",
             "django.contrib.contenttypes",
             "django.contrib.auth",
             "agon",
-        ),
+        ],
     )
 
 def runtests(*test_args):
