1321bb8ebd65498d84dab5dd5f7fb1634840b172
Patrick Altman <paltman@gmail.com>
2011-02-02 23:10:50 -0600

    0.1.dev15

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name = "agon",
-    version = "0.1.dev14",
+    version = "0.1.dev15",
     author = "Eldarion",
     author_email = "development@eldarion.com",
     description = "a reusable Django points, positions and levels application",
