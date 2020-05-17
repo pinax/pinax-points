92b2dcf33c7ebeec7854214eb8ebb8c5a0ea96ad
KatherineMichel <kthrnmichel@gmail.com>
2020-05-16 19:31:40 -0500

    Make version number standard

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-VERSION = "2.0"
+VERSION = "2.0.0"
 LONG_DESCRIPTION = """
 .. image:: http://pinaxproject.com/pinax-design/patches/pinax-points.svg
     :target: https://pypi.python.org/pypi/pinax-points/
